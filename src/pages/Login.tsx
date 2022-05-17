8aad5ee019c87dea19afa3d0233020f606bf64ab
Louis9211 <72727188+Louis9211@users.noreply.github.com>
2022-05-17 12:26:37 +0200

    New banner created, new component added

---
+++ src/pages/Login.tsx
@@ -0,0 +1,9 @@
+import React, { useState } from "react";
+
+const Login:React.FC = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const classes = useStyles();
+
+    const login =
+}
